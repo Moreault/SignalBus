2435181521ad3d31f9e14a8cc7ee6a3d3bcf1f06
Mathieu Moreault <seranx@gmail.com>
2024-06-01 19:08:43 -0400

    Updated to latest Dummies version

--- SignalBus.Tests/Garbage.cs
+++ SignalBus.Tests/Garbage.cs
@@ -1,6 +1,6 @@
 ﻿namespace ToolBX.SignalBus.Tests;
 
-public record Dummy
+public record Garbage
 {
     public string A { get; init; } = string.Empty;
     public string B { get; init; } = string.Empty;
